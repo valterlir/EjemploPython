0adc432b546db8d7109fa90baa316cf3eb339e64
valterlir <valterlira.m@gmail.com>
2025-06-09 16:26:05 -0400

    IncorporaResta

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,8 @@ print("calculadora pokemon")
 v1 = int(input("Valor 1 "))
 v2 = int(input("Valor 2 "))
 print("EL RESULTADO ES ",v1+v2)
+
+
+v1 = int(input("Valor 1 "))
+v2 = int(input("Valor 2 "))
+print("EL RESULTADO ES ",v1-v2)
